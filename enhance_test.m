cd53e9bfbde7b787fec754087d3aee8931799871
Aleksandar_M <137332355+Ten-Sharp@users.noreply.github.com>
2024-12-13 15:33:15 -0500

    Update enhance_test.m

--- enhance_test.m
+++ enhance_test.m
@@ -1,6 +1,13 @@
 I = imread("DB1_B\102_2.tif");
 
-
+% I = fingerprint image to process
+% normalization mean
+% normalization variance
+% blocksize
+% Mask variance threshold
+% Mask lower threshold
+% x wavelengths ratio
+% y wavelength ratio
 Im = fingerprint_enhancer(I,128,150,16,6,0.9,0.5,0.5);
 
 
