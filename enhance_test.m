0c48e17b70cc07ef36a7f3b598ae8aac607f7d2d
Aleksandar_M <137332355+Ten-Sharp@users.noreply.github.com>
2024-12-13 17:06:28 -0500

    pick images

--- enhance_test.m
+++ enhance_test.m
@@ -1,14 +1,36 @@
-I = imread("DB1_B\102_2.tif");
+[fileNames, filePath] = uigetfile({'*.jpg;*.png;*.bmp;*.tif', 'Image Files (*.jpg, *.png, *.bmp, *.tif)'}, ...
+    'Select Two Images', 'MultiSelect', 'on');
 
-% I = fingerprint image to process
-% normalization mean
-% normalization variance
-% blocksize
-% Mask variance threshold
-% Mask lower threshold
-% x wavelengths ratio
-% y wavelength ratio
-Im = fingerprint_enhancer(I,128,150,16,6,0.9,0.5,0.5);
+if isequal(fileNames, 0)
+    disp('User canceled the operation.');
+    return;
+end
+
+% Ensure two images are selected
+if length(fileNames) ~= 2
+    error('Please select exactly two matching fingerprints.');
+end
+
+for i = 1:2
+    imgPath = fullfile(filePath, fileNames{i});
+    Im = imread(imgPath);
+
+    dimensions = size(Im);
+
+    if length(dimensions) == 3
+        Im = rgb2gray(Im);
+    end
+
+    Im_enhanced = fingerprint_enhancer(Im,128,150,16,6,0.9,0.5,0.5);
+
+    [~, name, ext] = fileparts(fileNames{i});
+    newFileName = fullfile(filePath, [name, '_enhanced', ext]);
+
+    imwrite(Im_enhanced, newFileName);
+end
+
+
+% Im = fingerprint_enhancer(I,128,150,16,6,0.9,0.5,0.5);
 
 
 
